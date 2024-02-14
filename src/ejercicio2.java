dc11a6a889f902526445b86225aaae220e369b13
Patrik <psirb@myuax.com>
2024-02-14 11:34:37 +0100

    Bases. Creamos clases Red, dispositivo(computadoras, routers y switches)

--- src/ejercicio2.java
+++ src/ejercicio2.java
@@ -1,2 +1,49 @@
-public class ejercicio2 {
+import java.util.*;
+
+class Red{
+    List<Dispositivo> dispositivos = new ArrayList<>();
+
+    public Red() {
+        this.dispositivos = new ArrayList<>();
+    }
+
+    public void agregarDispositivo(Dispositivo dispositivo){
+        dispositivos.add(dispositivo);
+    }
 }
+
+abstract class Dispositivo{     //quiero routers, switches, computadoras, impresoras
+    String nombre;
+
+    public Dispositivo(String nombre) {
+        this.nombre = nombre;
+    }
+}
+
+class Router extends Dispositivo{
+    public Router(String nombre) {
+        super(nombre);
+    }
+}
+
+class Switch extends Dispositivo{
+    public Switch(String nombre) {
+        super(nombre);
+    }
+}
+
+class Computadora extends Dispositivo{
+    public Computadora(String nombre) {
+        super(nombre);
+    }
+}
+
+
+public class ejercicio2 {
+
+    public static void main(String[] args) {
+
+    }
+
+}
+
