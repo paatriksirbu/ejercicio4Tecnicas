26c0522256b981d4cdfa881140450dc2e802e348
Patrik <psirb@myuax.com>
2024-02-14 11:46:14 +0100

    clase Configuracion y funcion crearRed, crear dispositivos

--- src/ejercicio2.java
+++ src/ejercicio2.java
@@ -10,6 +10,16 @@ class Red{
     public void agregarDispositivo(Dispositivo dispositivo){
         dispositivos.add(dispositivo);
     }
+
+    public Map<Dispositivo, Double> simularTrafico(){
+        Map<Dispositivo, Double> traficoDispositivos = new HashMap<>();
+
+        for (Dispositivo dispositivo: dispositivos) {
+           double trafico = Math.random() * 100;
+              traficoDispositivos.put(dispositivo, trafico);
+        }
+        return traficoDispositivos;
+    }
 }
 
 abstract class Dispositivo{     //quiero routers, switches, computadoras, impresoras
@@ -38,8 +48,35 @@ class Computadora extends Dispositivo{
     }
 }
 
+class Configuracion{
+    int cantidadRouters;
+    int cantidadSwitches;
+    int cantidadComputadoras;
+
+    public Configuracion(int cantidadRouters, int cantidadSwitches, int cantidadComputadoras) {
+        this.cantidadRouters = cantidadRouters;
+        this.cantidadSwitches = cantidadSwitches;
+        this.cantidadComputadoras = cantidadComputadoras;
+    }
+}
+
 
 public class ejercicio2 {
+
+    public static Red crearRed(Configuracion configuracion){
+        Red red = new Red();
+        for (int i = 0; i < configuracion.cantidadRouters; i++) {
+            red.agregarDispositivo(new Router("Router" + i));
+        }
+        for (int i = 0; i < configuracion.cantidadSwitches; i++) {
+            red.agregarDispositivo(new Switch("Switch" + i));
+        }
+        for (int i = 0; i < configuracion.cantidadComputadoras; i++) {
+            red.agregarDispositivo(new Computadora("Computadora" + i));
+        }
+
+        return red;
+    }
 
     public static void main(String[] args) {
 
