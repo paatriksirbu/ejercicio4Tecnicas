891efb44abf96db5bf92fbe2c37e8fb78d433284
Patrik Paul Sirbu <psirb@myuax.com>
2024-02-16 21:21:15 +0100

    pequena modificacion y ejercicio 8, solo falta comentar

--- src/ejercicio7y8/BaseDatos.java
+++ src/ejercicio7y8/BaseDatos.java
@@ -8,15 +8,16 @@ public class BaseDatos {
     public BaseDatos() {
         citas = new ArrayList<Cita>();
 
-        citas.add(new Cita("Albert Einstein", "La vida es como andar en bicicleta. Para mantener el equilibrio, debes seguir adelante.", "En una carta a su hijo Eduard"));
-        citas.add(new Cita("Isaac Newton", "Si he logrado ver más lejos, ha sido porque he subido a hombros de gigantes.", "En una carta a Robert Hooke"));
-        citas.add(new Cita("Galileo Galilei", "E pur si muove.", "En una carta a Benedetto Castelli"));
-        citas.add(new Cita("Nikola Tesla", "Si quieres encontrar los secretos del universo, piensa en términos de energía, frecuencia y vibración.", "En una entrevista"));
-        citas.add(new Cita("Marie Curie", "Nada en la vida debe ser temido, solo comprendido. Ahora es el momento de comprender más, para que temamos menos.", "En una conferencia"));
-        citas.add(new Cita("Stephen Hawking", "La vida sería trágica si no fuera graciosa.", "En una entrevista"));
-        citas.add(new Cita("Richard Feynman", "La imaginación de la naturaleza es mucho, mucho mayor que la imaginación del hombre.", "En una conferencia"));
-        citas.add(new Cita("Carl Sagan", "Somos una forma de que el cosmos se conozca a sí mismo.", "En una entrevista"));
-        citas.add(new Cita("Neil deGrasse Tyson", "La buena ciencia es simplemente el aprendizaje de la realidad. La buena religión es simplemente la apreciación de la realidad.", "En una entrevista"));
+        citas.add(new Cita("Albert Einstein", "La vida es como andar en bicicleta. Para mantener el equilibrio, debes seguir adelante.", "Motivacion"));
+        citas.add(new Cita("Isaac Newton", "Si he logrado ver más lejos, ha sido porque he subido a hombros de gigantes.", "Inspiracion"));
+        citas.add(new Cita("Galileo Galilei", "E pur si muove.", "Ciencia"));
+        citas.add(new Cita("Nikola Tesla", "Si quieres encontrar los secretos del universo, piensa en términos de energía, frecuencia y vibración.", "Ciencia"));
+        citas.add(new Cita("Marie Curie", "Nada en la vida debe ser temido, solo comprendido. Ahora es el momento de comprender más, para que temamos menos.", "Motivacion"));
+        citas.add(new Cita("Stephen Hawking", "La vida sería trágica si no fuera graciosa.", "Reflexion"));
+        citas.add(new Cita("Richard Feynman", "La imaginación de la naturaleza es mucho, mucho mayor que la imaginación del hombre.", "Ciencia"));
+        citas.add(new Cita("Carl Sagan", "Somos una forma de que el cosmos se conozca a sí mismo.", "Exploracion"));
+        citas.add(new Cita("Neil deGrasse Tyson", "La buena ciencia es simplemente el aprendizaje de la realidad. La buena religión es simplemente la apreciación de la realidad.", "Reflexion"));
+
     }
 
     public List<Cita> getBDD() {
--- src/ejercicio7y8/MainContexto.java
+++ src/ejercicio7y8/MainContexto.java
@@ -1,4 +1,72 @@
 package ejercicio7y8;
+import java.util.*;
 
 public class MainContexto {
+
+    final static BaseDatos BDD = new BaseDatos();
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        boolean continuar = true;
+        while (continuar) {
+            String contexto = obtenerContexto(sc);
+            generarCitaContexto(contexto);
+            continuar = esperarSolicitudParaNuevaCita(sc);
+
+        }
+        sc.close();
+
+    }
+
+    public static List<Cita> obtenerCitasContexto(String contexto){
+        List<Cita> citasFiltradas = new ArrayList<Cita>();
+        for (Cita cita : BDD.getBDD()) {
+            if (cita.getContexto().equalsIgnoreCase(contexto)) {
+                citasFiltradas.add(cita);
+            }
+        }
+        return citasFiltradas;
+    }
+    public static void generarCitaContexto(String contexto) {
+        List<Cita> citasFiltradas = obtenerCitasContexto(contexto);
+        if (!citasFiltradas.isEmpty()) {
+            int r = obtenerCitaAleatoria(citasFiltradas.size());
+            generarCita(citasFiltradas.get(r));
+        } else {
+            System.out.println("Lo siento, no hay citas disponibles para este contexto.");
+        }
+    }
+
+    public static String obtenerContexto(Scanner sc){
+        System.out.println("Introduce el contexto de la cita que quieres obtener (Motivacion, Inspiracion, Ciencia, Reflexion, Exploracion)");
+        return sc.nextLine();
+    }
+
+    public static void generarCita(Cita cita){
+        System.out.println("Cita: " + cita);
+    }
+
+    public static boolean esperarSolicitudParaNuevaCita(Scanner sc){
+
+        System.out.println("Desea usted obtener otra cita inspiradora? (y/n)");
+        String respuesta = sc.nextLine();
+
+        if (respuesta.equalsIgnoreCase("n")){
+            System.out.println("Nos vemos pronto!");
+            return false;
+        } else if (respuesta.equalsIgnoreCase("y")) {
+            return true;
+        } else{
+            System.out.println("Ha sucedido un error");
+            return esperarSolicitudParaNuevaCita(sc);
+        }
+
+    }
+
+    public static int obtenerCitaAleatoria(int max){
+        Random rand = new Random();
+        return rand.nextInt(max);
+    }
+
+
 }
